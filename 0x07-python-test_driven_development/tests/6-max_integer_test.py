d8d23b5a6011be5276e34cefe985e19b290171ba
Adewale Fatoki <adewaleftk@gmail.com>
2023-02-02 20:29:48 +0100

    max integer corrected

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,20 +1,28 @@
-
-
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
+"""Unittest for max_integer"""
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+import unittest
+module = __import__("6-max_integer")
+max_integer = module.max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test class for max_integer"""
+
+    def test_module_docstring(self):
+        self.assertTrue(len(module.__doc__) > 1)
+
+    def test_function_docstring(self):
+        self.assertTrue(len(max_integer.__doc__) > 1)
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertEqual(max_integer([5, 3, 1, 4, 2]), 5)
+        self.assertEqual(max_integer([0, 2, 1, 5, 7, 4]), 7)
+        self.assertEqual(max_integer([-2, 0, -3, -5, -1]), 0)
+        self.assertEqual(max_integer([-2, -4, -3, -5, -1]), -1)
+        self.assertEqual(max_integer([-1, 1]), 1)
+        self.assertEqual(max_integer([-1]), -1)
+        self.assertEqual(max_integer(), None)
+        self.assertEqual(max_integer([]), None)

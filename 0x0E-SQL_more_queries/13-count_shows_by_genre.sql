f806ff83c64b484cbb1e9c2fe9ede4a2643df1b6
Adewale Fatoki <adewaleftk@gmail.com>
2023-03-19 14:23:29 +0100

    sql continued

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- list all genres and display the number of shows linked to each
+-- display tv_genres.name, number_of_shows
+-- order by number_of_shows (descending)
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;

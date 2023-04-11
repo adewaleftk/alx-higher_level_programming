b7cee599e59f33c899b915b237804c253aeb922b
Adewale Fatoki <adewaleftk@gmail.com>
2023-04-11 16:44:59 +0100

    Javascript Warm Up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const txt = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(txt);

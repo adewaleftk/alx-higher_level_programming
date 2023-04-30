10688997e7ee8d72c4f2425df13d22a6df0e42ac
Adewale Fatoki <adewaleftk@gmail.com>
2023-05-01 00:46:33 +0100

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""Fetch web response
+"""
 
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
-
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode('utf-8')))
-
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
+    print('\t- utf8 content: {}'.format(str(content)[2:-1]))
